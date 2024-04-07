3838982c16501874c5eeb5d67de3e02000cdd732
oliver <omcrobbie@gmail.com>
2024-04-07 14:00:42 -0400

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
 import Index from "./routes";
@@ -19,40 +24,39 @@ import {
   queryClient,
 } from "./util/functions";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: contactsLoader,
-    action: contactAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "/contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: favoriteAction,
-          },
-          {
-            path: "/contacts/:contactId/edit",
-            element: <EditContact />,
-            action: editAction,
-            loader: contactLoader,
-          },
-          {
-            path: "/contacts/:contactId/destroy",
-            action: deleteAction,
-            errorElement: <div>Oops! There was an error.</div>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      errorElement={<ErrorPage />}
+      loader={contactsLoader}
+      action={contactAction}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          errorElement={<ErrorPage />}
+          path="/contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={favoriteAction}
+        />
+        <Route
+          path="/contacts/:contactId/edit"
+          element={<EditContact />}
+          action={editAction}
+          loader={contactLoader}
+        />
+        <Route
+          path="/contacts/:contactId/destroy"
+          action={deleteAction}
+          errorElement={<div>Oops! There was an error.</div>}
+        />
+      </Route>
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
